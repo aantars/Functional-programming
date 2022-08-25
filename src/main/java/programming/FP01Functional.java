cf6bc0ef9cb98187e275721973d76d3e47863cad
Adolfo <aantars@hotmail.com>
2022-08-25 09:48:24 +0200

    adding filtering to the stream

--- src/main/java/programming/FP01Functional.java
+++ src/main/java/programming/FP01Functional.java
@@ -5,15 +5,22 @@ import java.util.List;
 
 public class FP01Functional {
      public static void main(String[] args){
-         printAListOFNumbersFunctional(Arrays.asList(2,4,6,8,12,14,4,8,15,2));
+         List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+
+         printAListOFNumbersFunctional(numbers);
      }
 
     private static void printAListOFNumbersFunctional(List<Integer> numbers) {
 
-         numbers.stream().forEach(FP01Functional::printNumber);
+         numbers.stream().filter(FP01Functional::isEven).forEach(System.out::println);
     }
 
-    private static void printNumber(Integer integer) {
+    private static boolean isEven(Integer integer) {
+         return integer%2==0;
+    }
+    }
+
+    /*private static void printNumber(Integer integer) {
          System.out.println(integer);
     }
-}
+}*/
